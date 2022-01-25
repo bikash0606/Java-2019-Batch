42470d8f9bebc2e6c4169f90a38abe06868c5ac8
bikash0606 <ojha.vikas0606@gmail.com>
2022-01-26 00:00:45 +0545

    CRUD from Eclipse added

---
+++ Assignments/Assignment10 JDBC/OnElipse/InsertEmpTbl.java
@@ -0,0 +1,22 @@
+package com.emp;
+
+import java.sql.*;
+
+public class InsertEmpTbl {
+    public static void main(String[] args) throws SQLException{
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crudop", "root", "Bikash@12345");
+
+            Statement st = con.createStatement();
+
+            String query = "INSERT INTO Emp(`EmpId`,`name`,`address`,`age`) VALUES (3,'Shyam','Kathmandu',22)";
+            
+            int rows = st.executeUpdate(query);
+             
+             System.out.println("affected row="+rows);
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
